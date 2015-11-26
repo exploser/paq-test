17de5aecc15a29d868890101943a3a04d7edc513
Pavel Vasilev <explosere@ya.ru>
2015-11-26 06:56:17 +0300

    Create paq-test.lua

---
+++ paq-test.lua
@@ -0,0 +1,9 @@
+io.write("Your paq is working fine! Type 'paq delete exploser paq-test' to remove this totally useless package\n")
+
+io.write("            __     __       \
+           /  \\~~~/  \\    \
+     ,----(     ..    ) \
+    /      \\__     __/   \
+   /|         (\\  |(\
+  ^ \\   /___\\  /\\ |   \
+     |__|   |__|-");
